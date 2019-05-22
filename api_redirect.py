fbf07e3cd0a9d561fb2a689f2153c3243a8a18b3
zfrankli <zfrankli@cisco.com>
2019-05-22 11:03:04 -0600

    generalize api redirect

--- api_redirect.py
+++ api_redirect.py
@@ -3,11 +3,11 @@ from flask import Flask, request
 app = Flask(__name__)
 target = 'http://localhost:8088'
 
-@app.route('/jabber', defaults={'path': ''})
-@app.route('/jabber/<path:path>')
+@app.route('/api', defaults={'path': ''})
+@app.route('/api/<path:path>')
 def catch_all(path):
     return requests.get('/'.join((target, path)), headers=request.headers).text
 
 
 if __name__ == '__main__':
-   app.run(port=8021)
+   app.run(port=8080)
